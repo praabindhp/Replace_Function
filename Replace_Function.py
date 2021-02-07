86ae1508e6dd604d55c719398e174ade7f7d1cd4
praabindhp <praabindhp@gmail.com>
2021-02-07 16:52:00 +0530

    Modified

---
+++ Replace_Function.py
@@ -0,0 +1,12 @@
+# Welcome Message
+print("\nWelcome, This Is Praabindh")
+# Intro Message
+print("Illustrating An Example Of The Replace Function In Python")
+# String Input From The User
+s1=input("\nEnter Your Content ")
+# Getting The Word To Be Found From The User's Content
+w1=input("\nEnter The Word To Be Found In Your Content ")
+# Getting The Word To Be Replaced From The User's Content
+w2=input("\nEnter The Word To Be Replaced In Your Content ")
+# Printing The Modified Content
+print(s1.replace(w1, w2))
